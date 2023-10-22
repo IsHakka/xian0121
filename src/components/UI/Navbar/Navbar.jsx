5fab4a8c5129a5964b163484318f8c2ac28d66e4
Xian <qaq81704@gmail.com>
2023-10-22 20:10:37 +0800

    test✅:

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { menu, close, logo } from '../../../assets';
-import { Link, NavLink } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import classes from './Navbar.module.scss'
 import { navLinks } from '../../../constants/index'
 
